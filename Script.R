24a48e04ea2a1f7af904363ed10eccdf4eab4fef
thilex12 <thilex12@gmail.com>
2024-06-10 10:24:05 +0200

    debut tranfso

--- Script.R
+++ Script.R
@@ -4,3 +4,18 @@ test <- c(1:10)
 #okokok
 
 #blablabla
+#library(readxl)
+open(encoding='UTF-8')
+data <- read.csv("Patrimoine_Arbore.csv", locale = locale(encoding = "UTF-8"))
+data$clc_quartier = iconv(data$clc_quartier,from = "latin1" , to="UTF-8")
+data$clc_secteur = iconv(data$clc_secteur,from = "latin1" , to="UTF-8")
+data$fk_arb_etat = iconv(data$fk_arb_etat,from = "latin1" , to="UTF-8")
+data$commentaire_environnement = iconv(data$commentaire_environnement,from = "latin1" , to="UTF-8")
+
+for (col in data) {
+  data$col = iconv(col,from = "latin1" , to="UTF-8")
+}
+
+View(data)
+
+summary(data)
