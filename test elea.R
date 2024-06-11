7ec86c0d3871f730a24be2e19a86e07431191368
elealecointe <elea.lecointe@isen-ouest.yncrea.fr>
2024-06-11 15:42:51 +0200

    elra

--- test elea.R
+++ test elea.R
@@ -42,6 +42,8 @@ ggplot(data, aes(x = clc_quartier, y = haut_tot)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+
+
 a = as.Date(as.character(1), format = "%Y")
 a = as.numeric(a)
 a = as.Date(a)
