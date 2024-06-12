4d36a8a80a6e8159f54f8985f7e9d20e280df486
thilex12 <thilex12@gmail.com>
2024-06-12 21:59:22 +0200

    maps

--- map.R
+++ map.R
@@ -78,13 +78,104 @@ map_arbre <- function(data) {
 
 # map_arbre(data)
 
-install.packages("devtools")
 
-library(devtools)
-install_github("wch/webshot")
 
-library(htmlwidgets)
-library(webshot)
+
+
+map_arbre_etat <- function(data) {
+    
+
+    install.packages("sf")
+    install.packages("dplyr")
+    install.packages("leaflet")
+
+    library(sf)
+    library(dplyr)
+    library(leaflet)
+
+    etats <- unique(data$fk_arb_etat)
+    colors <- colorFactor(palette = rainbow(length(etats)), levels = etats)
+
+    points_sf <- st_as_sf(data.frame(x = c(data$X), y = c(data$Y)),
+                          coords = c("x", "y"),
+                          crs = 3949)
+    points_transformed <- st_transform(points_sf, 4326)
+    # points_transformed
+
+    coords <- st_coordinates(points_transformed)
+    long <- coords[, 1]
+    lat <- coords[, 2]
+
+    data_map <- data.frame(x = long, y = lat) %>%
+        st_as_sf(coords = c("x", "y"), crs = 4326)
+
+
+    data_map %>%
+      leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
+      addTiles() %>%
+      addCircles(radius = 2,
+                 color = colors(data$fk_arb_etat),
+                 popup = ~paste("ID :", data$id_arbre,
+                                "<br>Quartier :", data$clc_quartier,
+                                "<br>Secteur :", data$clc_secteur,
+                                "<br>Etat :", data$fk_arb_etat,
+                                "<br> Remarquable :", data$remarquable)
+                ) %>%
+      addLegend(position = "bottomright",
+      colors = colors(etats),
+      labels = etats,
+      title = "Etat de l'arbre")
+    # return(data_map)
+}
+
+
+map_arbre_stadedev <- function(data) {
+    
+
+    install.packages("sf")
+    install.packages("dplyr")
+    install.packages("leaflet")
+
+    library(sf)
+    library(dplyr)
+    library(leaflet)
+
+    stadedev <- unique(data$fk_stadedev)
+    colors <- colorFactor(palette = rainbow(length(stadedev)), levels = stadedev)
+
+    points_sf <- st_as_sf(data.frame(x = c(data$X), y = c(data$Y)),
+                          coords = c("x", "y"),
+                          crs = 3949)
+    points_transformed <- st_transform(points_sf, 4326)
+    # points_transformed
+
+    coords <- st_coordinates(points_transformed)
+    long <- coords[, 1]
+    lat <- coords[, 2]
+
+    data_map <- data.frame(x = long, y = lat) %>%
+        st_as_sf(coords = c("x", "y"), crs = 4326)
+
+
+    data_map %>%
+      leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
+      addTiles() %>%
+      addCircles(radius = 2,
+                 color = colors(data$fk_stadedev),
+                 popup = ~paste("ID :", data$id_arbre,
+                                "<br>Quartier :", data$clc_quartier,
+                                "<br>Secteur :", data$clc_secteur,
+                                "<br>Etat :", data$fk_arb_etat,
+                                "<br> Remarquable :", data$remarquable)
+                ) %>%
+      addLegend(position = "bottomright",
+      colors = colors(stadedev),
+      labels = stadedev,
+      title = "Stade de devleoppement")
+    # return(data_map)
+}
+
+
 
 #Save map .html & .png
 saveWidget(map_arbre(data), "temp.html", selfcontained = FALSE)
@@ -92,8 +183,21 @@ webshot("temp.html", file = "Rplot.png", cliprect = "viewport")
 
 
 map_web <- function(map){
+    install.packages("devtools")
+
+    library(devtools)
+    install_github("wch/webshot")
+
+    library(htmlwidgets)
+    library(webshot)
     saveWidget(map, "temp.html", selfcontained = FALSE)
     webshot("temp.html", file = "Rplot.png", cliprect = "viewport")
 }
 
-map_web(map_arbre(data))
+# map_web(map_arbre(data))
+# map_web(map_arbre_etat(data))
+map_web(map_arbre_stadedev(data))
+
+
+
+
