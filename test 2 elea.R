fd45956de23b8a4daac04f73550d20212f5e857f
elealecointe <elea.lecointe@isen-ouest.yncrea.fr>
2024-06-12 13:35:44 +0200

    bgrfng

--- test 2 elea.R
+++ test 2 elea.R
@@ -1,11 +1,14 @@
 source("Script.R")
-data <- traitement(read.csv("Patrimoine_Arbore_modif.csv", dec='.',sep=','))
+data <- traitement(read.csv("Patrimoine_Arbore_modif.csv", dec='.',sep=',')) #séparateur décimal et séparateur de champ
 
 View(data$fk_arb_etat)
 
 #Boxplot de la hauteur totale des arbres par quartier
-ggplot(data, aes(x = clc_quartier, y = haut_tot)) +
-  geom_boxplot() +
+data_quartier<-data[data$clc_quartier!="inconnu",] #ne prend pas en compte les valeurs "inconnu"
+data_quartier$clc_quartier<-droplevels(data_quartier$clc_quartier)
+
+ggplot(data_quartier, aes(x = clc_quartier, y = haut_tot)) +
+  geom_boxplot() + #pour les boites à moutache
   labs(title = "Boxplot de la hauteur totale des arbres par quartier",
        x = "Quartiers",
        y = "Hauteurs Totales") +
@@ -13,7 +16,10 @@ ggplot(data, aes(x = clc_quartier, y = haut_tot)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) #pour que nos titres de quartiers ne se superposent pas
 
 #distribution des types de feuillage par quartier
-ggplot(data, aes(x = clc_quartier, fill = feuillage)) +
+data_feuillage<-data[data$feuillage!="inconnu",]
+data_feuillage$feuillage<-droplevels(data_feuillage$feuillage)
+
+ggplot(data_feuillage, aes(x = clc_quartier, fill = feuillage)) +
   geom_bar(position = "dodge") +
   labs(title = "Distribution des types de feuillage par quartier",
        x = "Quartiers",
@@ -63,11 +69,12 @@ testchi2
 mosaicplot(contingency_table, main = "Mosaicplot des états des arbres en fonction du type de sol",
            xlab = "Type de sol (fk_pied)", ylab = "État de l'arbre (fk_arb_etat)",
            color = rainbow(length(unique(data$fk_arb_etat))),
-           las=3, par(mai=3))
+           las=2)#place correctement les labels horizontalement/verticalement 
 
 
 #test chi2 pour corrélation entre le quartier et le feuillage
-contingency_table2 <- table(data$clc_quartier, data$feuillage)
+table_filtree2<-data[data$feuillage != 'inconnu', ] #enleve toutes les données inconnues pour exécuter notre chi2
+contingency_table2 <- table(table_filtree2$clc_quartier, table_filtree2$feuillage)
 print(contingency_table2)
 
 testchi2_2 <- chisq.test(data$clc_quartier, data$feuillage)
@@ -78,7 +85,17 @@ testchi2_2
 mosaicplot(contingency_table2, main = "Mosaicplot des feuillages en fonction des quartiers",
            xlab = "Quartiers (clc_quartier)", ylab = "Feuillages (feuillage) ",
            color = rainbow(length(unique(data$fk_arb_etat))),
-           las=3)
+           las=2)
+
+
+#régression logistique => quels sont les arbres à abbatre
+model<- glm(data$fk_arb_etat!="EN PLACE"~ data$age + data$fk_stadedev + data$remarquable, family="binomial")
+summary(model)
+resultat<-predict(model, data.frame(data),type="response")
+plot(resultat)
+arbre_a_abbatre<-resultat[resultat>0.6]
+arbre_a_abbatre
+
 
 
 
